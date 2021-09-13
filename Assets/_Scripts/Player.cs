24586c94f9aed806acf3ee5218db6aedafa0378e
lohannahme <61636785+lohannahme@users.noreply.github.com>
2021-09-13 05:33:20 -0300

    Testando conexao

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mirror {
+    public class Player : NetworkBehaviour
+    {
+
+        [SerializeField]
+        private Vector3 movement = new Vector3();
+        
+        [Client]
+        void Update()
+        {
+            if (!hasAuthority) { return; }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Translate(movement);
+            }
+
+        }
+    }
+}
